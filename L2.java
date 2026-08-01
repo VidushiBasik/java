fe9cd0ab8f18316738b1c952ce856bfb0ee570bb
VidushiBasik <vidushibasik@gmail.com>
2026-08-01 21:34:02 +0530

    auto commit

---
+++ L2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class L2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int fact = 1;
+        System.out.println("Enter a number :");
+        num = sc.nextInt();
+        for(i=1 ;i<=num ;i++ ){
+            fact *= i;
+        }
+        System.out.println("Factorial : "+fact);
+    }
+}
