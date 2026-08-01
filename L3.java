5e62091963fce9c8a30abd75022c0f4926c5c198
VidushiBasik <vidushibasik@gmail.com>
2026-08-01 21:40:00 +0530

    auto commit

---
+++ L3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class L3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int tab=1;
+        for(int i=1;i<=10;i++){
+            tab=num*i;
+        }
+        System.out.println("Table of "+num+" is "+tab);
+    }
+}
