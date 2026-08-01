20cdd4bd2931ea65fcdc063740e40f7e0512cf89
VidushiBasik <vidushibasik@gmail.com>
2026-08-01 21:42:37 +0530

    auto commit

--- L3.java
+++ L3.java
@@ -2,11 +2,10 @@ import java.util.*;
 public class L3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num;
-        int tab=1;
         for(int i=1;i<=10;i++){
-            tab=num*i;
+            int num = sc.nextInt();
+            int tab=num*i;
         }
-        System.out.println("Table of "+num+" is "+tab);
+        System.out.println(num+" x "+i+" = "+ tab);
     }
 }
