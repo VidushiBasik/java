006c666a437803a529aa145d00a0fa036d87f34a
VidushiBasik <vidushibasik@gmail.com>
2026-08-01 21:43:24 +0530

    auto commit

--- L3.java
+++ L3.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class L3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num :");
+        int num = sc.nextInt();
         for(int i=1;i<=10;i++){
-            int num = sc.nextInt();
             int tab=num*i;
         }
         System.out.println(num+" x "+i+" = "+ tab);
