595d16502adb7130c6591f3612cb467ce8ad763e
VidushiBasik <vidushibasik@gmail.com>
2026-08-01 21:20:47 +0530

    test

--- L1.java
+++ L1.java
@@ -1,17 +1,23 @@
 import java.util.*;
 public class L1{
-    public static void main (String[] args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int even=0, odd=0;
-        while(sc.hasNextInt()){
-            int n = sc.nextInt();
-            if(n%2==0){
-                even++;
-            } else {
-                odd++;
+        int num;
+        int choice;
+        int even=0;
+        int odd=0;
+        do { 
+            System.out.println("Enter a num: ");
+            num = sc.nextInt();
+            if (num%2==0){
+                even+=num;
+            }else{
+                odd+=num;
             }
-        }
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+            System.out.println("Enter 1 if you want to continue");
+            choice = sc.nextInt();
+        } while (choice==1);
+            System.out.println("Even sum is "+even);
+            System.out.println("Odd sum is "+odd);
     }
 }
