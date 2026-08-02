68bd03f89b2e7f9ccbd3af9861fa1af7e9aae23f
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 08:54:03 +0530

    auto commit

--- nl3.java
+++ nl3.java
@@ -1,9 +1,11 @@
 public class nl3 {
     public static void main(String[] args){
-        char n='A';
+        int n = 4;
+        char ch ='A';
         for(char i=1;i<=n;i++){
             for(char j=1;j<=i;j++){
-                System.out.print(n);
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
