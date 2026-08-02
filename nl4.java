569c60f19fd8f40b4977efe381bb80aa7cf645c8
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 09:37:33 +0530

    auto commit

--- nl4.java
+++ nl4.java
@@ -5,7 +5,7 @@ public class nl4{
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for (int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print("Enter the number of lines "+j);
             }
             System.out.println();
         }
