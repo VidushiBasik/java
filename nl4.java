8b85830e45f6cd1ca87def45b9e4b48fc9e437a0
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 09:33:41 +0530

    auto commit

--- nl4.java
+++ nl4.java
@@ -1,10 +1,11 @@
 public class nl4 {
     public static void main(String[] args){
     int n = 4;
-    int m = 4;
+    int m = 1;
     for (int i = 1; i <=n; i++) {
         for(int j=1;j<=m;j++){
-            System.out.print(n);
+            System.out.print(m);
+            m++;
         }
         System.out.println();
     }
