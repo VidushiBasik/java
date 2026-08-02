f3d1bbfa99e60a8be73f78b832b9e09227ae9676
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 08:47:06 +0530

    auto commit

--- loop3.java
+++ loop3.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class loop3 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        try(Scanner sc = new Scanner(System.in)){
         int range = sc.nextInt();
         int num = 1;
         while(num<=range){
             System.out.print(num+" ");
             num++;
+        }
         }
     }
 }
