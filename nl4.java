4ece44e4b8c47f2cd4ce87e80d9e5d7db7fabb7b
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 09:42:25 +0530

    auto commit

--- nl4.java
+++ nl4.java
@@ -4,8 +4,8 @@ public class nl4{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of lines you want to print : ");
         int n = sc.nextInt();
-        for(int i=0;i<=n;i++){
-            for (int j=2;j<=i;j++){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
                 System.out.print(j);
                 j++;
             }
