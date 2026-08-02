180d060a53231b1fba808e6c58216ac5cb5ecc2f
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 09:37:01 +0530

    auto commit

--- nl4.java
+++ nl4.java
@@ -1,13 +1,13 @@
-public class nl4 {
+import java.util.*;
+public class nl4{
     public static void main(String[] args){
-    int n = 4;
-    int m = 1;
-    for (int i = 1; i>=n; i++) {
-        for(int j=1;j>=m;j++){
-            System.out.print(m);
-            m++;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
     }
 }
