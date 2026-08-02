f33fcba204e4f5722e34cdaf56e87842fe2e1317
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 08:26:20 +0530

    auto commit

--- nl1.java
+++ nl1.java
@@ -7,7 +7,7 @@ public class nl1{
             for(int j =1;j<=i;j++){
                 System.out.print(" * ");
             }
+            System.out.println();
         }
-        System.out.println();
     }
 }
