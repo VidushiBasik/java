55f5de8f8f4692519b2c7e5512f8af287fa062fb
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 09:38:28 +0530

    auto commit

--- nl4.java
+++ nl4.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class nl4{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of lines you want to print");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for (int j=1;j<=i;j++){
-                System.out.print("Enter the number of lines "+j);
+                System.out.print(j);
             }
             System.out.println();
         }
