c0278c81d768e77f0d926c8e8216c5c0c5ee2625
VidushiBasik <vidushibasik@gmail.com>
2026-08-02 08:33:17 +0530

    auto commit

--- nl2.java
+++ nl2.java
@@ -4,7 +4,7 @@ public class nl2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i-n+1;j++){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print("*");
             }
             System.out.println();
